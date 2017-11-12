5a13ad14a31d296772abc784441253952c3b95b5
Xamenyap <jpham2811@gmail.com>
2017-11-12 21:50:09 +0700

    Initialize repository

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <increment-button></increment-button>
+    <increment-button></increment-button>
+  </div>
+</template>
+
+<script>
+import IncrementButton from './components/IncrementButton.vue';
+
+export default {
+  name: 'app',
+  components: { IncrementButton },
+  data() {
+    return {};
+  }
+}
+</script>
---
+++ src/components/IncrementButton.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<span></span><br/>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				message: 'Hello'
+			};
+		}
+	}
+</script
